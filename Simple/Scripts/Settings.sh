c1947ba8048ea2b4cd8da05f9ebef7221fd81ce4
cca2878 <1054331608@qq.com>
2024-11-21 07:58:40 +0800

    Test

--- Simple/Scripts/Settings.sh
+++ Simple/Scripts/Settings.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
 #添加编译日期标识
+echo $WRT_CI-$WRT_DATE
 sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ $WRT_CI-$WRT_DATE')/g" $(find ./feeds/luci/modules/luci-mod-status/ -type f -name "10_system.js")
