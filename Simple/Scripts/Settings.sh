bc9e9546625f7557bb5306da81f469a2e54c4a30
cca2878 <1054331608@qq.com>
2024-11-21 08:21:39 +0800

    Update files

--- Simple/Scripts/Settings.sh
+++ Simple/Scripts/Settings.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #添加编译日期标识
-echo $WRT_CI-$WRT_DATE
-sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ $WRT_CI-$WRT_DATE')/g" $(find ./feeds/luci/modules/luci-mod-status/ -type f -name "10_system.js")
+export WRT_TAG=$WRT_CI'_'$WRT_DATE
+echo $WRT_TAG
+sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ $WRT_TAG')/g" $(find ./feeds/luci/modules/luci-mod-status/ -type f -name "10_system.js")
