b2daa20b25a02ad35f7e83fbcd20279d51d2ff4a
cca2878 <1054331608@qq.com>
2024-11-24 04:27:09 +0800

    Update workflow, scripts & Add noSSP conf

--- Simple/Scripts/Settings.sh
+++ Simple/Scripts/Settings.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#添加编译日期标识
-export WRT_TAG=$WRT_TARGET'_'$WRT_DATE
+#添加固件标识
 echo $WRT_TAG
 sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ $WRT_TAG')/g" $(find ./feeds/luci/modules/luci-mod-status/ -type f -name "10_system.js")
